dc71c22cc88b2cdfc6a9dbe86caeae477543b0ce
yanhuan <1428563923@qq.com>
2018-03-07 20:31:18 +0800

    login perfection

--- src/main/java/com/yh/index/LoginController.java
+++ src/main/java/com/yh/index/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yh.entity.User;
 import com.yh.user.service.UserService;
@@ -29,13 +30,25 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value = "login", method = RequestMethod.POST)
+	@ResponseBody
 	public Map<String,Object> login(HttpServletRequest request, HttpSession session) {
 		String account = request.getParameter("account");
 		String password = request.getParameter("password");
-		String remember=request.getParameter("remember-me");
-		User user=userService.login(account, password);
+		//String remember=request.getParameter("remember-me");
 		Map<String,Object> map=new HashMap<>();
-		if(user!=null&&user.equals("")) {
+		if(account.isEmpty() || account==null) {
+			map.put("code", "0");
+			map.put("msg", "用户名为空");
+			return map;
+		}
+		if(password.isEmpty() || password==null) {
+			map.put("code", "0");
+			map.put("msg", "密码为空");
+			return map;
+		}
+		User user=userService.login(account, password);
+		
+		if(user==null&&user.equals("")) {
 			map.put("code", "0");
 			map.put("msg", "用户名错误");
 			return map;
@@ -43,9 +56,9 @@ public class LoginController {
 		session.setAttribute("user", user);
 		map.put("code", "1");
 		map.put("msg", "登录成功");
-		if(remember.equals("true")) {
+		/*if(remember.equals("true")) {
 			//cookie  存入cookie
-		}
+		}*/
 		return map;
 	}
 		
